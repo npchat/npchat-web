3c4545ab8141d56f9b065025c3e5e8596af7ca3a
Joseph Innes <joseph.innes@swissinfo.ch>
2021-10-09 13:49:19 +0200

    Clean up

--- src/index.mjs
+++ src/index.mjs
@@ -56,8 +56,8 @@ async function handleRequest(request, env) {
 		opts.status = 400
 		return new Response(JSON.stringify({error:"Missing sigPubJwkHash"}), opts)
 	}
-  const id = env.CHANNEL.idFromName(sigPubJwkHash);
-  const obj = env.CHANNEL.get(id);
+  const id = env.CHANNEL.idFromName(sigPubJwkHash)
+  const obj = env.CHANNEL.get(id)
 	if (request.method === "GET"){
 		return await obj.fetch(request.url, {headers: request.headers})
 	}
@@ -113,7 +113,7 @@ export class Channel {
 	async handleGetChallenge() {
 		const challenge = await this.state.storage.get(challengeKey)
 		if (!challenge || this.hasChallengeExpired(JSON.parse(challenge))) {
-			const newChallenge = JSON.stringify(this.makeChallenge());
+			const newChallenge = JSON.stringify(this.makeChallenge())
 			this.state.storage.put(challengeKey, newChallenge)
 			return new Response(newChallenge, defaultResponseOpts())
 		}
@@ -238,96 +238,96 @@ export class Channel {
 	base58() {
 		// https://github.com/45678/Base58
 		const result = {}
-		const ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-		const ALPHABET_MAP = {};
-		let i = 0;
+		const ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+		const ALPHABET_MAP = {}
+		let i = 0
 	
 		while (i < ALPHABET.length) {
 			ALPHABET_MAP[ALPHABET.charAt(i)] = i;
-			i++;
+			i++
 		}
 	
 		result.encode = buffer => {
-			let carry, digits, j;
+			let carry, digits, j
 			if (buffer.length === 0) {
-				return "";
+				return ""
 			}
-			i = void 0;
-			j = void 0;
-			digits = [0];
-			i = 0;
+			i = void 0
+			j = void 0
+			digits = [0]
+			i = 0
 			while (i < buffer.length) {
-				j = 0;
+				j = 0
 				while (j < digits.length) {
-					digits[j] <<= 8;
-					j++;
+					digits[j] <<= 8
+					j++
 				}
-				digits[0] += buffer[i];
-				carry = 0;
-				j = 0;
+				digits[0] += buffer[i]
+				carry = 0
+				j = 0
 				while (j < digits.length) {
-					digits[j] += carry;
-					carry = (digits[j] / 58) | 0;
-					digits[j] %= 58;
-					++j;
+					digits[j] += carry
+					carry = (digits[j] / 58) | 0
+					digits[j] %= 58
+					++j
 				}
 				while (carry) {
-					digits.push(carry % 58);
-					carry = (carry / 58) | 0;
+					digits.push(carry % 58)
+					carry = (carry / 58) | 0
 				}
-				i++;
+				i++
 			}
-			i = 0;
+			i = 0
 			while (buffer[i] === 0 && i < buffer.length - 1) {
-				digits.push(0);
-				i++;
+				digits.push(0)
+				i++
 			}
 			return digits.reverse().map(function(digit) {
-				return ALPHABET[digit];
-			}).join("");
+				return ALPHABET[digit]
+			}).join("")
 		};
 	
 		result.decode = string => {
-			let bytes, c, carry, j;
+			let bytes, c, carry, j
 			if (string.length === 0) {
-				return new(typeof Uint8Array !== "undefined" && Uint8Array !== null ? Uint8Array : Buffer)(0);
+				return new Uint8Array(0)
 			}
-			i = void 0;
-			j = void 0;
-			bytes = [0];
-			i = 0;
+			i = void 0
+			j = void 0
+			bytes = [0]
+			i = 0
 			while (i < string.length) {
-				c = string[i];
+				c = string[i]
 				if (!(c in ALPHABET_MAP)) {
-					throw "Base58.decode received unacceptable input. Character '" + c + "' is not in the Base58 alphabet.";
+					throw "Base58.decode received unacceptable input. Character '" + c + "' is not in the Base58 alphabet."
 				}
-				j = 0;
+				j = 0
 				while (j < bytes.length) {
-					bytes[j] *= 58;
-					j++;
+					bytes[j] *= 58
+					j++
 				}
-				bytes[0] += ALPHABET_MAP[c];
-				carry = 0;
-				j = 0;
+				bytes[0] += ALPHABET_MAP[c]
+				carry = 0
+				j = 0
 				while (j < bytes.length) {
-					bytes[j] += carry;
-					carry = bytes[j] >> 8;
-					bytes[j] &= 0xff;
-					++j;
+					bytes[j] += carry
+					carry = bytes[j] >> 8
+					bytes[j] &= 0xff
+					++j
 				}
 				while (carry) {
-					bytes.push(carry & 0xff);
-					carry >>= 8;
+					bytes.push(carry & 0xff)
+					carry >>= 8
 				}
-				i++;
+				i++
 			}
-			i = 0;
+			i = 0
 			while (string[i] === "1" && i < string.length - 1) {
-				bytes.push(0);
-				i++;
+				bytes.push(0)
+				i++
 			}
-			return new(typeof Uint8Array !== "undefined" && Uint8Array !== null ? Uint8Array : Buffer)(bytes.reverse());
-		};
+			return new Uint8Array(bytes.reverse())
+		}
 	
 		return result
 	}
