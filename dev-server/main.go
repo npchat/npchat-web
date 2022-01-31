0982dd2f0b9a135d2370fe8f38c9401c753b5a97
Joseph Innes <joeinnes0@gmail.com>
2022-01-31 23:09:53 +0100

    Use own dev server that supports https

---
+++ dev-server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var port int
+	var certFile string
+	var keyFile string
+	flag.IntVar(&port, "port", 8000, "must be an int")
+	flag.StringVar(&certFile, "cert", "", "must be a file path")
+	flag.StringVar(&keyFile, "key", "", "must be a file path")
+	flag.Parse()
+
+	addr := fmt.Sprintf(":%v", port)
+	log.Println("Listening on " + addr)
+
+	fs := http.FileServer(http.Dir("../www"))
+
+	var err error
+
+	if certFile != "" && keyFile != "" {
+		log.Println("Expecting HTTPS connections")
+		err = http.ListenAndServeTLS(addr, certFile, keyFile, fs)
+	} else {
+		err = http.ListenAndServe(addr, fs)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- web-dev-server.config.js
+++
@@ -1,8 +0,0 @@
-export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
-  rootDir: "www",
-  port: 8001,
-  watch: true,
-  nodeResolve: {
-    exportConditions: ["browser", "development"]
-  }
-})
